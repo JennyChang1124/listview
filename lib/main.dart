e87ac88449a15198e8303726a5ef271251757256
張茜雯 <112846217+JennyChang1124@users.noreply.github.com>
2022-12-05 10:28:45 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'ListView'),
     );
   }
 }
@@ -32,30 +32,27 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
 
-
-
   @override
   Widget build(BuildContext context) {
-    final List<String> entries = <String>['A', 'B', 'C'];
-    final List<int> colorCodes = <int>[600, 500, 100];
+    const items = <String>['第一項','第二項','第三項'];
+    const icons = <String>['assets/11.png','assets/2.png','assets/33.png'];
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-            child:ListView.separated(
-              padding: const EdgeInsets.all(8),
-              itemCount: entries.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 50,
-                  color: Colors.amber[colorCodes[index]],
-                  child: Center(child: Text('Entry ${entries[index]}')),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) => const Divider(),
-            )
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      );
+    body: Center(
+          child: ListView.separated(
+          itemCount: items.length,
+              itemBuilder: (context,index) =>
+          ListTile(title:Text(items[index], style: TextStyle(fontSize: 20),),
+        onTap: () => Text('點選' + items[index]),
+        leading: Container(
+        child: CircleAvatar(backgroundImage: AssetImage(icons[index],),),
+        padding: EdgeInsets.symmetric(vertical: 8,horizontal: 10),),
+        subtitle: Text('每項說明',style: TextStyle(fontSize: 16),),),
+        separatorBuilder: (context, index) => Divider(),
+    )
+    ),
+    );
   }
 }
